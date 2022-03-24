7546e30580cef68fd8de7a6ff0c96331a2b2c468
Dewashish Mehta <dewashish3596@yahoo.com>
2022-03-24 14:02:54 +0530

    rectified errors

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,60 @@
 import 'package:acadmy/screens/mainpage.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:animated_splash_screen/animated_splash_screen.dart';
 
 import './screens/splash.dart';
+import 'screens/login_page.dart';
 import 'widgets/utils.dart';
 
 final navigatorKey = GlobalKey<NavigatorState>();
-void main() => runApp(MyApp());
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      scaffoldMessengerKey: Utils.messengerKey,
+      navigatorKey: navigatorKey,
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: StreamBuilder<User?>(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            } else if (snapshot.hasError) {
+              return const Center(
+                child: Text("Something went wrong!!!"),
+              );
+            } else if (snapshot.hasData) {
+              return MainPage();
+            } else {
+              return LoginPage();
+            }
+          },
+        ),
+      ),
+    );
+  }
+}
+/*void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -27,7 +75,7 @@ class _MyAppState extends State<MyApp> {
     );
   }
 }
-
+*/
 /*  home: AnimatedSplashScreen(
         splash: Splash(),
         nextScreen: MainPage(),
